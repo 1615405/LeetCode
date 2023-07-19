89f2acbaf36bc171e3f09f7b9676751b5f8ef6dc
cherry <1615405@qq.com>
2023-07-19 11:07:42 +0800

    modify files

--- 02.二分/0034. 在排序数组中查找元素的第一个和最后一个位置.py
+++ 02.二分/0034. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,16 +1,15 @@
 def lower_bound(nums: List[int], target: int) -> int:
-    left, right = 0, len(nums) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if nums[mid] < target:
-            left = mid + 1
+    left, right = 0, len(nums)
+    while left < right:
+        pivot = (left + right) // 2
+        if nums[pivot] >= target:
+            right = pivot
         else:
-            right = mid - 1
+            left = pivot + 1
     return left
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        '''
         start = lower_bound(nums, target)
         if start == len(nums) or nums[start] != target:
             return [-1, -1]
@@ -22,6 +21,7 @@ class Solution:
             return [-1, -1]
         end = bisect_right(nums, target) - 1
         return [start, end]
+        '''
 
 
 
@@ -29,35 +29,38 @@ class Solution:
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if (nums.empty()) {
-            return {-1, -1};
-        }
-        int left = 0, right = nums.size() - 1;
-        int index1 = -1, index2 =-1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        function<int(void)> bsearch_1 = [&](void) -> int {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int pivot = left + (right - left) / 2;
+                if (nums[pivot] >= target) {
+                    right = pivot;
+                } else {
+                    left = pivot + 1;
+                }
             }
-        }
-        index1 = left;
-
-        left = 0, right = nums.size() - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2 + 1;
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid;
-            }
-        }
-        index2 = left;
+            return left;
+        };
         
-        if (nums[index1] != target || nums[index2] != target) {
-            return {-1, -1};
+        function<int(void)> bsearch_2 = [&](void) -> int {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int pivot = left + (right - left) / 2 + 1;
+                if (nums[pivot] > target) {
+                    right = pivot - 1;
+                } else {
+                    left = pivot;
+                }
+            }
+            return left;
+        };
+
+        int x = bsearch_1();
+        int y = bsearch_2();
+        
+        if (x == (int)nums.size() || nums[x] != target) {
+            return vector<int>{-1, -1};
         }
-        return {index1, index2};
+        return vector<int>{x, y};
     }
 };
