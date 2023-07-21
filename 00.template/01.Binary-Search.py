5f687949cc18b8d053954e3ab4df37f29aea8939
cherry <1615405@qq.com>
2023-07-21 10:10:17 +0800

    modify files

--- 00.template/01.Binary-Search.py
+++ 00.template/01.Binary-Search.py
@@ -1,4 +1,4 @@
-bool check(int x) {/* ... */}  # 检查 x 是否满足某种性质
+bool is_blue(int x) {/* ... */}  # 红蓝染色法
 
 # 区间[l, r]被划分成[l, mid]和[mid + 1, r]时使用：
 int bsearch_1(int l, int r)
@@ -6,7 +6,7 @@ int bsearch_1(int l, int r)
     while (l < r)
     {
         int mid = l + r >> 1;
-        if (check(mid)) r = mid;    # check()判断 mid 是否满足性质
+        if (is_blue(mid)) r = mid;    # mid 属于蓝色, right = mid
         else l = mid + 1;
     }
     return l;
@@ -18,7 +18,7 @@ int bsearch_2(int l, int r)
     while (l < r)
     {
         int mid = l + r + 1 >> 1;
-        if (check(mid)) l = mid;
+        if (is_red(mid)) l = mid; # mid 属于蓝色, l = mid
         else r = mid - 1;
     }
     return l;
